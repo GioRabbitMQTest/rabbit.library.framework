b0c8b3b334a1dbe60350d67f8db1cb2f73ca6115
giomen601 <arizamendozagiovany@gmail.com>
2025-02-25 20:46:37 -0500

    masstransient

---
+++ src/framework/rabbit.framework.messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace rabbit.framework.messaging.Events;
+public record IntegrationEvent
+{
+  public Guid Id => Guid.NewGuid();
+  public DateTime OcurrendOn => DateTime.Now;
+  public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/framework/rabbit.framework.messaging/MassTransit/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace rabbit.framework.messaging.MassTransit;
+public static class Extensions
+{
+  public static IServiceCollection AddMessageBroker
+  (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+  {
+    services.AddMassTransit(config =>
+    {
+      config.SetKebabCaseEndpointNameFormatter();
+      if (assembly != null)
+        config.AddConsumers(assembly);
+
+      config.UsingRabbitMq((context, configurator) =>
+      {
+        configurator.Host(new Uri(configuration["MessageBroker:Host"]!), hots =>
+        {
+          hots.Username(configuration["MessageBroker:UserName"]!);
+          hots.Password(configuration["MessageBroker:Password"]!);
+        });
+        configurator.ConfigureEndpoints(context);
+      });
+    });
+    return services;
+  }
+}
