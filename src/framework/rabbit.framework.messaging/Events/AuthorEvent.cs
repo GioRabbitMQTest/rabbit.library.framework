005ec9e0cd111bc034d387e3dc8ad1f199fdac4e
giomen601 <arizamendozagiovany@gmail.com>
2025-03-21 08:03:57 -0500

    update

---
+++ src/framework/rabbit.framework.messaging/Events/AuthorEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace rabbit.framework.messaging.Events;
+public record AuthorEvent : IntegrationEvent
+{
+  public string FirtName { get; set; }
+  public string LastName { get; set; }
+  public string PhoneNumber { get; set; }
+  public string Email { get; set; }
+  public bool IsActive { get; set; }
+}
